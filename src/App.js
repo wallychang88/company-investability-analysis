41bcce62a4e284b9aa17d57fad78b64c08ebca9c
wallychang88 <71866671+wallychang88@users.noreply.github.com>
2025-05-07 16:02:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1029,7 +1029,7 @@ return (
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
             </svg>
             <span className="mt-2 text-base text-gray-600">{file ? file.name : "Click to select a file or drop it here"}</span>
-            <span className="text-xs text-gray-500 mt-1">CSV/TSV format with company data</span>
+            <span className="text-xs text-gray-500 mt-1">Raw SourceScrub CSV/TSV format</span>
           </label>
         </section>
 
